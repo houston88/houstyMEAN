13292f1f106537d6b8f0c5e9fb8df9d73a5234c5
hharris3 <houston_harris@intuit.com>
2014-01-31 20:16:31 -0600

    readme and forever daemon scripts

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Have to run grunt first to build everything
+# grunt --force if you are running remote
+
+export PORT="80"
+export NODE_ENV="production"
+forever start -l ~/foreverLog.txt -o ~/serverLog.txt -e ~/errorLog.txt dist/server.js
+
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+forever stop dist/server.js
+
