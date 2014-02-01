9fd90bbebc7ea3cfe5cac5c657f7bf5be7b0e8af
hharris3 <houston_harris@intuit.com>
2014-02-01 00:01:18 -0600

    Fixes for carousel. Switched to js for prev next to prevent accidental routing.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -3,5 +3,15 @@
 angular.module('houstyApp')
 .controller('HomeCtrl', function ($scope) {
   $scope.hello = 'Hello Home';
+  
+  // Cant use #hrefs with angular routing on
+  // Use JavaScript controls for carousel
+  $scope.prevSlide = function() {
+    angular.element('.carousel').carousel('prev');
+  };
+  $scope.nextSlide = function() {
+    angular.element('.carousel').carousel('next');
+  };
+  
 });
   
